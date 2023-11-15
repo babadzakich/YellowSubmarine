02ba49fbf20b9d2ce155a820825ae822301d7eca
babadzakich <doctorare@mail.ru>
2023-11-15 19:02:10 +0700

    added some commentaries on merge sort

--- 9_Pack/mergeSort.c
+++ 9_Pack/mergeSort.c
@@ -4,12 +4,13 @@
 
 void merge(int *array, int left, int mid, int right) {
 
-    int lengthOne = mid - left + 1;
-    int lengthTwo = right - mid;
+    int lengthOne = mid - left + 1; // 1 massiv levih
+    int lengthTwo = right - mid;// 2 massiv pravih
  
     int* leftArray = (int*)malloc(sizeof(int)*lengthOne);
     int* rightArray = (int*)malloc(sizeof(int)*lengthTwo);
- 
+
+    //filling arrays with numbers
     for (int step = 0; step < lengthOne; step++)
     {
         leftArray[step] = array[left + step];
@@ -21,10 +22,10 @@ void merge(int *array, int left, int mid, int right) {
     }
 
     int leftStep = 0, rightStep = 0, arrayStep = left;
-    
+    //while arrays are full
     while (leftStep < lengthOne && rightStep < lengthTwo) 
     {
-        if (leftArray[leftStep] <= rightArray[rightStep]) 
+        if (leftArray[leftStep] <= rightArray[rightStep]) // if left < right we add to our result array left, else we add right
         {
             array[arrayStep++] = leftArray[leftStep++];
         }
@@ -33,7 +34,8 @@ void merge(int *array, int left, int mid, int right) {
             array[arrayStep++] = rightArray[rightStep++];
         }
     }
- 
+    //when we run out of one array, we check if we have some values in another array,
+    // and because of comparasion, we can add all remaining values without checking
     while (leftStep < lengthOne)
     {
         array[arrayStep++] = leftArray[leftStep++];
@@ -50,9 +52,9 @@ void merge(int *array, int left, int mid, int right) {
 void mergeSort(int* array, int left, int right) {
     if (left < right) 
     {
-        int mid = left + (right - left) / 2;
+        int mid = left + (right - left) / 2;//creating partition index
  
-        mergeSort(array, left, mid);
+        mergeSort(array, left, mid); //with recursion we divide one array into two 4 -> 2,2 -> 1,1,1,1, and then we merge them with merge
         mergeSort(array, mid + 1, right);
  
         merge(array, left, mid, right);
