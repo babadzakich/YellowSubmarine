de2ae6379c1a48f071ddd3dcc69105add40d82f6
babadzakich <doctorare@mail.ru>
2023-11-15 11:19:04 +0700

    made quicksort

--- 9_Pack/quicksort.c
+++ 9_Pack/quicksort.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <conio.h>
+#include <time.h>
 
 
 int randomNum(const int left, const int right) 
@@ -11,9 +12,15 @@ int randomNum(const int left, const int right)
 
 void quicksort(int* array, int leftStart, int rightStart)
 {
-    int left = leftStart, right = rightStart-1;
-    int pivot = array[(leftStart+rightStart)/2];
-    do 
+    if (leftStart > rightStart)
+    {
+        return;
+    }
+    int left = leftStart, right = rightStart;
+    srand(time(NULL));
+    int pivot = array[rand() % (right - left + 1) + left];
+
+    while (left <= right)
     {
         while (array[left] < pivot)
         {
@@ -25,45 +32,43 @@ void quicksort(int* array, int leftStart, int rightStart)
         }
         if (left <= right)
         {
-            if (array[left] > array[right])
-            {
-                int temporary = array[left];
-                array[left] = array[right];
-                array[right] = temporary;   
-            }  
-        }
-        left++;
-        if(right>0)
-        {
+            int temporary = array[left];
+            array[left] = array[right];
+            array[right] = temporary;   
+            left++;
             right--;
         }
-    }while (left <= right);
-    if (left < rightStart)
-    {
-        quicksort(array, left, rightStart);
     }
-    if (right > leftStart)
-    {
-        quicksort(array, leftStart, right);
-    }
+    quicksort(array, leftStart, right);
+    quicksort(array, left, rightStart);
+    
 }
 
 int main()
 {
+    // int a[] = {1,2,2,2,1};
+    // quicksort(a,0,4);
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     printf("%d ",a[i]);
+    // }
+    
     FILE *in = fopen("input.txt", "rb");
     FILE *out = fopen("output.txt", "wb");
 
     int32_t arrayLength, maxNum = 0;
     fread(&arrayLength, sizeof(int), 1, in);
-    int32_t array[arrayLength];
-    for (int step = 0; step < arrayLength; step++)
-    {
-        fread(&array[step], sizeof(int), 1, in);
-    }
-    quicksort(array, 0, arrayLength);
-    for (int step = 0; step < arrayLength; step++)
-    {
-        fwrite(&array[step], sizeof(int), 1, out);
-    }
+
+    int32_t* array = (int32_t*)malloc(sizeof(int32_t) * arrayLength);
+
+    fread(array, sizeof(int32_t), arrayLength, in);
+    
+    quicksort(array, 0, arrayLength-1);
+    
+    fwrite(array, sizeof(int), arrayLength, out);
+    
+    free(array);
+    fclose(in);
+    fclose(out);
     return 0;
 }
