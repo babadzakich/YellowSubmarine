e4f3cb29171dbdfa0dd53ddd4e236f8c8405efd3
babadzakich <doctorare@mail.ru>
2023-12-09 22:00:28 +0700

    pseudo random fkng destroyed and casted into oblivion

--- 11_Pack/pseudoRandom.c
+++ 11_Pack/pseudoRandom.c
@@ -1,27 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#define maxLimit 9369319
 
-uint64_t func(uint64_t s, uint64_t a, uint64_t b, uint64_t c, uint64_t M)
+typedef struct Node_s
 {
-    return ( s * s * a + s * b + c ) % M ;
+    struct Node_s *next;
+    uint64_t value;
+    uint64_t counter;
+}Node;
+
+
+int isHere(Node** hashTable, uint64_t hash, uint64_t value) 
+{
+    Node* temp = hashTable[hash];
+    while (temp != NULL) 
+    {
+        if (temp->value == value) 
+        {
+            return 1;
+        }
+        temp = temp->next;
+    }
+    return 0;
+}
+
+uint64_t a, b, c, M;
+uint64_t func(uint64_t s)
+{
+    return ( s * s * a + s * b + c ) % M;
 }
 
 int main()
 {
-    int a, b, c, M;
-    scanf("%d", &M);
-    scanf("%d %d %d", &a, &b, &c);
-    int state = 1;
-    int* array = (int*)malloc(sizeof(int)* 2 * 1000001);
-    for(int step = 0; step < 2000002; step++)
+    scanf("%llu", &M);
+    scanf("%llu %llu %llu", &a, &b, &c);
+    uint64_t state = 1, counter = 0;
+    uint64_t hash = state % maxLimit;
+    Node** hashTable = (Node**)calloc(maxLimit,sizeof(Node*));
+    while(counter < M)
     {
-        array[step] = 0;
-    }   
-    int head = -1, tail = -1;
-    while (tail < 0)
-    {
-        int hash = func(state, a, b, c, M);
+        if(isHere(hashTable, hash, state))
+        {
+            break;
+        }
+        Node* new = (Node*)malloc(sizeof(Node));
+        new->counter = counter;
+        new->value = state; 
+        new->next = NULL;
+        if(hashTable[hash] == NULL)
+        {
+            hashTable[hash] = new;
+        }
+        else
+        {
+            Node* current = hashTable[hash];
+            while(current->next != NULL)
+            {
+                current = current->next;
+            }
+            current->next = new;
+        }
+        state = func(state);
+        hash = state % maxLimit;
+        counter++;
     }
+    
+    Node* result = hashTable[hash];
+    while(result->value != state)
+    {
+        result = result->next;
+    }
+    printf("%llu %llu\n", result->counter ,counter);
+    for(int step = 0; step < maxLimit; step++)
+    {
+        if (hashTable[step]!= NULL)
+
+        {
+            Node* current = hashTable[step];
+            Node* temp = NULL;
+            while(current != NULL)
+            {
+                temp = current;
+                current = current->next;
+                free(temp);
+            }
+        }
+        
+    }
+    free(hashTable);
     return 0;
 }
